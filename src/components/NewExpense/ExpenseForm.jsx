f4339625a6e22058f3f216c423ca2d1cb8d6d883
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-07 07:22:39 +0530

    Add changes for each useState

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
   return (
     <form action="POST">
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" />
+          <input type="text" name="title" onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" name="amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            name="amount"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
 
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" name="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            name="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
+        <button
+          type="submit"
+          onClick={() => console.log(enteredTitle, enteredDate, enteredAmount)}
+        >
+          Add Expense
+        </button>
       </div>
     </form>
   );
