4e810919aefb2a2e0cf498cf19956c18fa367777
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-11 17:19:45 +0530

    Original assignment solution

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,31 +3,38 @@ import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../../UI/Card";
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
 
 function Expense({ expenses }) {
+  const onFilterYearHandler = (year) => {
+    console.log(year);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
-    </Card>
+    <div>
+      <ExpensesFilter onFilterYear={onFilterYearHandler} />
+      <Card className="expenses">
+        <ExpenseItem
+          title={expenses[0].title}
+          date={expenses[0].date}
+          amount={expenses[0].amount}
+        />
+        <ExpenseItem
+          title={expenses[1].title}
+          date={expenses[1].date}
+          amount={expenses[1].amount}
+        />
+        <ExpenseItem
+          title={expenses[2].title}
+          date={expenses[2].date}
+          amount={expenses[2].amount}
+        />
+        <ExpenseItem
+          title={expenses[3].title}
+          date={expenses[3].date}
+          amount={expenses[3].amount}
+        />
+      </Card>
+    </div>
   );
 }
 
