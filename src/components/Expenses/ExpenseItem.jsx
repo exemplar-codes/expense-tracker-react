c1e9fe63764f2438450cfb72bca8939fd9acf0fc
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-12 23:07:07 +0530

    Make JSX semantically better

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -7,14 +7,16 @@ import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">$ {props.amount}</div>
-        <button>Change title</button>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">$ {props.amount}</div>
+          <button>Change title</button>
+        </div>
+      </Card>
+    </li>
   );
 }
 
