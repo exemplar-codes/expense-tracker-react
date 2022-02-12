14b8b6aa5b7115a622a2cc46c3f48d06587cdc03
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-02-12 23:11:04 +0530

    Remove button

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -13,7 +13,6 @@ function ExpenseItem(props) {
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">$ {props.amount}</div>
-          <button>Change title</button>
         </div>
       </Card>
     </li>
